243b23e6beceecb379029c51d03917791398e0d4
seongbeom lim <seongbeom1234@gmail.com>
2022-03-04 16:33:31 +0900

    Update clock.js
    실시간 시간 표시

--- JS/clock.js
+++ JS/clock.js
@@ -1,1 +1,12 @@
 const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(getClock, 1000);
+getClock();
